0abd8c60528274db7de27036418450cfb4cf84f8
Allen Zhang <allen.zhang018@gmail.com>
2020-09-26 22:18:57 +1000

    add recommend

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <div class="title">Where to go in weekend</div>
+    <ul>
+      <li
+        class="item border-bottom"
+        v-for="item of recommendList"
+        :key="item.id"
+      >
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{ item.title }}</p>
+          <p class="item-desc">{{ item.desc }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data() {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl:
+            'http://img1.qunarzz.com/sight/source/1505/aa/7baaf8a851d221.jpg_r_640x214_1431200f.jpg',
+          title: 'Dalian Sea World',
+          desc: 'A romantic sea world theme park'
+        },
+        {
+          id: '0002',
+          imgUrl:
+            'http://img1.qunarzz.com/sight/source/1505/aa/7baaf8a851d221.jpg_r_640x214_1431200f.jpg',
+          title: 'Dalian Sea World',
+          desc: 'A romantic sea world theme park'
+        },
+        {
+          id: '0003',
+          imgUrl:
+            'http://img1.qunarzz.com/sight/source/1505/aa/7baaf8a851d221.jpg_r_640x214_1431200f.jpg',
+          title: 'Dalian Sea World',
+          desc: 'A romantic sea world theme park'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl';
+.title
+  margin-top .2rem
+  line-height .8rem
+  background #eee
+  text-indent .2rem
+.item-img-wrapper
+  overflow hidden
+  height 0
+  padding-bottom 33.9%
+.item-img
+  width 100%
+.item-info
+  padding .1rem
+  .item-title
+    line-height .54rem
+    font-size .32rem
+    ellipsis()
+  .item-desc
+    line-height .4rem
+    color #ccc
+    ellipsis()
+  .item-button
+    line-height .44rem
+    margin-top .16rem
+    background #ff9300
+    padding 0 .2rem
+    border-radius .06rem
+    color #fff
+</style>
