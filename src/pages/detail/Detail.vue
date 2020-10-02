0af0140ecf1a4f342d56f2380c9c7e5468b006c8
Allen Zhang <allen.zhang018@gmail.com>
2020-10-03 01:40:20 +1000

    detail list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,17 +2,35 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
+    <detail-list :list="list"></detail-list>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data() {
+    return {
+      list: [
+        {
+          title: 'Adult ticket'
+        },
+        {
+          title: 'Student ticket'
+        },
+        {
+          title: 'Child ticket'
+        }
+      ]
+    }
   }
 }
 </script>
